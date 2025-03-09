0dff9d1d6a99d4e42331a86ed02276806a1d88da
Glen Sargent <glensarg@hotmail.co.uk>
2025-03-09 13:05:04 +0000

    chore: helloworld with positions

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,38 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main where
 
+import Aztecs
+import qualified Aztecs.ECS.Access as A
+import qualified Aztecs.ECS.Query as Q
+import qualified Aztecs.ECS.System as S
+import Control.DeepSeq
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.Function
+import GHC.Generics
+
+newtype Position = Position Int deriving (Show, Generic, NFData)
+
+instance Component Position
+
+newtype Velocity = Velocity Int deriving (Show, Generic, NFData)
+
+instance Component Velocity
+
+move :: (Monad m) => QueryT m Position
+move = Q.fetch & Q.adjust (\(Velocity v) (Position p) -> Position $ p + v)
+
+run :: AccessT IO ()
+run = do
+  positions <- S.map move
+  liftIO $ print positions
+
+app :: AccessT IO ()
+app = do
+  A.spawn_ $ bundle (Position 0) <> bundle (Velocity 1)
+  forever run
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runAccessT_ app
